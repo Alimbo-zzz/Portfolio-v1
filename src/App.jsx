3065c4e5b8eb4a322e5458bccbf33a613b718935
Alim Mambetov <alimbo333@gmail.com>
2023-08-20 05:11:48 +0300

    hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import {Landing, Error, Admin} from '@pages';
-import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
+import { Landing, Error, Admin } from '@pages';
+import { Route, RouterProvider, createBrowserRouter, createHashRouter, createRoutesFromElements } from 'react-router-dom';
 
 
 
 const App = (props) => {
-	
 
-	const router = createBrowserRouter(createRoutesFromElements(<>
-		<Route path='/' element={<Landing/>}/>
-		<Route path='/admin/*' element={<Admin/>}/>
-		<Route path='*' element={<Error/>}/>
+
+	const router = createHashRouter(createRoutesFromElements(<>
+		<Route path='/' element={<Landing />} />
+		<Route path='/admin/*' element={<Admin />} />
+		<Route path='*' element={<Error />} />
 	</>))
 
-	
+
 	return (<>
-		<RouterProvider router={router}/>
+		<RouterProvider router={router} />
 	</>);
 }
 
